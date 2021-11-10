0e63a0b57f3f604ff13b57729b4133baf890dfee
Karen Kitchens <karen.kitchens@gmail.com>
2021-11-10 10:32:41 -0600

    Add initial react app for Like a Local

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+// get our fontawesome imports
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Like a L<FontAwesomeIcon icon={faMapMarkerAlt} />cal
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
